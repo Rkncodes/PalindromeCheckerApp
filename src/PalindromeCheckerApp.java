bc77548c9808e3856602ad4d702a6390fed1db2a
Rkncodes <rajvinderkaurpersonal@gmail.com>
2026-03-18 14:22:26 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,11 +2,17 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("     PALINDROME CHECKER APP      ");
-        System.out.println("=================================");
-        System.out.println("Version : 1.0.0");
-        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("=== PALINDROME CHECKER APP ===");
 
+        // UC2 — Hardcoded Palindrome
+        String word = "madam";
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
