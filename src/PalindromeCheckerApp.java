d9734a1433ce091b907197181776c36a67bbee36
Rkncodes <rajvinderkaurpersonal@gmail.com>
2026-03-18 14:46:46 +0530

    UC10 - Case-insensitive & space ignored palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,30 @@
-public class UC9_RecursiveCheck {
+public class UC10_CaseInsensitiveCheck {
 
-    public static boolean isPalindrome(String str, int left, int right) {
+    public static boolean isPalindrome(String input) {
 
-        // Base case
-        if (left >= right)
-            return true;
+        // Remove spaces & convert to lowercase
+        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
 
-        // If characters not equal → not palindrome
-        if (str.charAt(left) != str.charAt(right))
-            return false;
+        int left = 0;
+        int right = cleaned.length() - 1;
 
-        // Recursive call
-        return isPalindrome(str, left + 1, right - 1);
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
 
-        String word = "racecar";
+        String text = "A man a plan a canal Panama";
 
-        boolean result = isPalindrome(word, 0, word.length() - 1);
+        boolean result = isPalindrome(text);
 
-        System.out.println(word + (result ? " is Palindrome" : " is NOT Palindrome"));
+        System.out.println("\"" + text + "\"" +
+                (result ? " is Palindrome" : " is NOT Palindrome"));
     }
 }
