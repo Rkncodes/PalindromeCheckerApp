730b7c6082bc1767ad285612dc541708375691a8
Rkncodes <rajvinderkaurpersonal@gmail.com>
2026-03-18 14:19:25 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,6 +7,6 @@ public class PalindromeCheckerApp {
         System.out.println("=================================");
         System.out.println("Version : 1.0.0");
         System.out.println("Welcome to the Palindrome Checker Application!");
-        System.out.println("Program ready...");
+
     }
 }
